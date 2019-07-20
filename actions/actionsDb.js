d587ca1b9f53733fd900bd1db30f6e608686e7d7
Josemaria <josemarianriagu25@gmail.com>
2019-07-21 00:41:00 +0800

    refactor module exports

--- actions/actionsDb.js
+++ actions/actionsDb.js
@@ -2,45 +2,40 @@ const db = require('../data/dbConfig');
 const mappers = require('../helpers/mappers')
 
 module.exports = {
-   getActions,
-   getActionById,
-   insertAction,
-   updateAction,
-   removeAction
-};
+   getActions: async function () {
+      const query = await db('actions');
+      mappers.refiner(query);
+      return query;
+   },
 
-async function getActions() {
-   const query = await db('actions');
-   mappers.refiner(query);
-   return query;
-}
+   getActionById: async function (id) {
+      let actions = await db('actions as a')
+         .where('a.id', id);
 
-async function getActionById(id) {
-   let actions = await db('actions as a')
-      .where('a.id', id);
+      if (actions.length !== 0) {
+         mappers.refiner(actions);
+         let [action] = actions // destructure the single action from the actions array
+         return action;
+      }
+      return null
+   },
 
-   if (actions.length !== 0) {
-      mappers.refiner(actions);
-      return actions;
+   insertAction: function (action) {
+      return db('actions')
+         .insert(action)
+         .then(([id]) => this.getActionById(id));
+   },
+
+   updateAction: function (id, changes) {
+      return db('actions')
+         .where('id', id)
+         .update(changes)
+         .then(count => (count > 0 ? this.getActionById(id) : null));
+   },
+
+   removeAction: function (id) {
+      return db('actions')
+         .where('id', id)
+         .del();
    }
-   return null
-}
-
-function insertAction(action) {
-   return db('actions')
-      .insert(action)
-      .then(([id]) => getActionById(id));
-}
-
-function updateAction(id, changes) {
-   return db('actions')
-      .where('id', id)
-      .update(changes)
-      .then(count => (count > 0 ? getActionById(id) : null));
-}
-
-function removeAction(id) {
-   return db('actions')
-      .where('id', id)
-      .del();
-}
+};
