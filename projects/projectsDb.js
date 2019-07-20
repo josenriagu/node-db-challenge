c11b33f57b0ea8ea771e6b9c37dcee778001ac45
Josemaria <josemarianriagu25@gmail.com>
2019-07-20 23:57:40 +0800

    refine getProjectById helper function

--- projects/projectsDb.js
+++ projects/projectsDb.js
@@ -25,16 +25,12 @@ async function getProjectById(id) {
    let projects = await db('projects as p')
       .where('p.id', id);
    let projectActions = await getProjectActions(id);
+
    if (projects.length !== 0) {
       mappers.refiner(projects);
       mappers.refiner(projectActions);
-      let result = {
-         id: projects[0].id,
-         name: projects[0].name,
-         description: projects[0].description,
-         completed: projects[0].completed,
-         actions: projectActions
-      }
+      let [project] = projects // destructure the single project in the array
+      let result = { ...project, actions: projectActions }
       return result;
    }
    return null
